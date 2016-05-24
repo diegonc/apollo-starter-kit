deb3ea34988eb74dfdf68e7cf8633ece5ed043f2
Diego Nieto Cid <dnietoc@gmail.com>
2016-05-23 23:49:49 -0300

    Add type using unions

--- data/schema.js
+++ data/schema.js
@@ -1,7 +1,27 @@
 const typeDefinitions = `
 type Query {
   testString: String
+  aUser: User
 }
+
+type User {
+  id: ID!
+  credentials: Credentials
+}
+
+type EmailAccount {
+  typename: String!
+  email: String!
+  password: String!
+}
+
+type GitHubAccount {
+  typename: String!
+  handle: String!
+  token: ID!
+}
+
+union Credentials = EmailAccount | GitHubAccount
 
 schema {
   query: Query
