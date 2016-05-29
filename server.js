723b7b7682c81787c4307df854af030107e78081
Diego Nieto Cid <dnietoc@gmail.com>
2016-05-29 14:51:54 -0300

    Add resolver for union type

--- server.js
+++ server.js
@@ -5,11 +5,18 @@ import Mocks from './data/mocks';
 
 const GRAPHQL_PORT = 8080;
 
+const resolvers = {
+	Credentials: {
+		__resolveType: (data, ctx, info) => info.schema.getType(data.typename)
+	}
+}
+
 const graphQLServer = express();
 graphQLServer.use('/', apolloServer({
   graphiql: true,
   pretty: true,
   schema: Schema,
+  resolvers,
   mocks: Mocks,
 }));
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
